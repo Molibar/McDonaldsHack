8135719617404d190a93ad8b5a04b098c0419d20
Molibar <christer.brannstrom@gmail.com>
2014-11-22 04:11:10 +0000

    Got the Mongo Spinning.

--- McDonaldsHack.Api/Controllers/ActionController.cs
+++ McDonaldsHack.Api/Controllers/ActionController.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Configuration;
+using System.Linq;
 using System.Web.Http;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
 
 namespace McDonaldsHack.Api.Controllers
 {
@@ -8,15 +13,61 @@ namespace McDonaldsHack.Api.Controllers
     {
         [HttpGet]
         [Route("api/Login/{name}/{password}")]
-        public LoginViewModel CheckIn(string name, string password)
+        public LoginViewModel Login(string name, string password)
         {
             var token = Guid.NewGuid();
-            return new LoginViewModel {Name = name, Password = password, Token = token.ToString()};
+            var database = DatabaseRepository.GetDatabase();
+            var user = new User
+            {
+                Name = name,
+                Password = password,
+                Token = token.ToString()
+            };
+            var collection = database.GetCollection<User>(user.GetType().ToString());
+            collection.Insert(user);
+            return new LoginViewModel { Name = user.Name, Password = user.Password, Token = user.Token };
+        }
+
+        [HttpGet]
+        [Route("api/User/{token}")]
+        public LoginViewModel GetUser(string token)
+        {
+            var database = DatabaseRepository.GetDatabase();
+            var collection = database.GetCollection<User>(typeof(User).ToString());
+            var query =
+                from e in collection.AsQueryable()
+                where e.Token == token
+                select e;
+            var user = query.FirstOrDefault();
+            if (user == null) return null;
+            return new LoginViewModel { Name = user.Name, Password = user.Password, Token = user.Token };
+        }
+    }
+
+    public static class DatabaseRepository
+    {
+        public static MongoDatabase GetDatabase()
+        {
+            var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+            var url = new MongoUrl(connectionstring);
+            var client = new MongoClient(url);
+            var server = client.GetServer();
+            var database = server.GetDatabase("McDonaldsHack");
+            return database;
         }
     }
 
     public class LoginViewModel
     {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+    }
+
+
+    public class User
+    {
+        public ObjectId Id { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
         public string Token { get; set; }
