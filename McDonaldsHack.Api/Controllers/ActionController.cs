3c997a676a13fae26a8350805ee6212793ea9a48
Molibar <christer.brannstrom@gmail.com>
2014-11-22 02:16:07 +0000

    Login Update

--- McDonaldsHack.Api/Controllers/ActionController.cs
+++ McDonaldsHack.Api/Controllers/ActionController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.Entity;
 using System.Web.Http;
 
 namespace McDonaldsHack.Api.Controllers
@@ -7,53 +6,19 @@ namespace McDonaldsHack.Api.Controllers
     [AcceptCors]
     public class LoginController : ApiController
     {
-
         [HttpGet]
-        [Route("api/LogIn/{name}/{password}")]
-        public dynamic CheckIn(string name, string password)
+        [Route("api/Login/{name}/{password}")]
+        public LoginViewModel CheckIn(string name, string password)
         {
             var token = Guid.NewGuid();
-            return new {name, password, token };
-        }
-
-        [HttpGet]
-        [Route("api/Scan/{missionId}/{characterName}")]
-        public Game Scan(int missionId, string characterName)
-        {
-            if (Database.Game == null) Database.Game = Game.Init();
-            Database.Game.FinishMission(characterName, missionId);
-            return Database.Game;
-        }
-
-        [HttpGet]
-        [Route("api/CheckOut/{characterName}")]
-        public Game CheckOut(string characterName)
-        {
-            if (Database.Game == null) Database.Game = Game.Init();
-            Database.Game.CheckOut(characterName);
-            return Database.Game;
+            return new LoginViewModel {Name = name, Password = password, Token = token.ToString()};
         }
     }
 
-    [AcceptCors]
-    public class ScoreController : ApiController
+    public class LoginViewModel
     {
-
-        [HttpGet]
-        [Route("api/Scores/{table}")]
-        public List<HighScore> Get(string table)
-        {
-            if (Database.HighScores == null) return null;
-            return Database.HighScores[table];
-        }
-
-        [HttpPost]
-        [Route("api/Scores/{table}")]
-        public List<HighScore> Post(string table, string name, int score)
-        {
-            if (Database.Game == null) Database.Game = Game.Init();
-            Database.HighScores[table].Add(new HighScore { Name = name, Score = score });
-            return Database.HighScores[table];
-        }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
     }
 }
